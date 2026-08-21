97330c05e55a31203d9a7d028daee5e11750d67a
Divya2001-raj <68492709+Divya2001-raj@users.noreply.github.com>
2026-08-22 04:27:10 +0530

    Add: duplicate-integer - submission-42

---
+++ Data Structures & Algorithms/duplicate-integer/submission-42.py
@@ -0,0 +1,22 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashset= set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+
+        # nums.sort()
+        # for i in range(1,len(nums)):
+        #     if nums[i]==nums[i-1]:
+        #         return True
+        # return False
+        
+        # return len(set(nums))!=len(nums)
+
+        # nums.sort()
+        # for i in range(len(nums)-1):
+        #     if nums[i]==nums[i+1]:
+        #         return True
+        # return False
